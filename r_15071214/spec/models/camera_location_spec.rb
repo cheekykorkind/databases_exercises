889daeef902ff9d7bafeba2b3b80d26a58b19d99
cheekykorkind <cheekorkind@gmail.com>
2023-10-29 02:39:32 -0400

    테이블(2차 정리)처럼 create가능한가 확인하는 테스트 추가

---
+++ r_15071214/spec/models/camera_location_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe CameraLocation do
+  describe '#create' do
+    context '부모 관계인 Facility와 WhichDirection를 미리 만들지 않아서' do
+      let(:create_param) {{location: 'location1', facility_id: 1, which_direction_id: 1}}
+      let(:expected) {"Validation failed: Facility must exist, Which direction must exist"}
+      it 'create 불가능' do
+        expect { CameraLocation.create!(create_param) }.to raise_error(ActiveRecord::RecordInvalid, expected)
+      end
+    end
+
+    context '부모 관계인 Facility와 WhichDirection를 미리 만들면' do
+      # Facility
+      let(:national_police_agency_id) {NationalPoliceAgency.create!({name: 'p_agency1'}).id}
+      let(:police_station_id) {PoliceStation.create!({name: 'p_station1', national_police_agency_id:}).id}
+      let(:management_agency_id) {ManagementAgency.create!({name: "m_agency1", national_police_agency_id:, police_station_id:}).id}
+      let(:purpose_of_camera_installation_id) {PurposeOfCameraInstallation.create!({purpose: "purpose1"}).id}
+      let(:facility_id) {Facility.create!({name: 'facility1', management_agency_id:, purpose_of_camera_installation_id:}).id}
+      # WhichDirection
+      let(:which_direction_id) {WhichDirection.create!({direction: 'direction1'}).id}
+      # CameraLocation
+      let(:create_param) {{location: 'location1', facility_id:, which_direction_id:}}
+      let(:expected) {1}
+      it 'create 가능' do
+        CameraLocation.create!(create_param)
+        actual = CameraLocation.all.length
+
+        expect(actual).to equal(expected)
+      end
+    end
+  end
+end
---
+++ r_15071214/spec/models/facility_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Facility do
+  describe '#create' do
+    context '부모 관계인 ManagementAgency와 PurposeOfCameraInstallation를 미리 만들지 않아서' do
+      let(:create_param) {{name: 'facility1', number_of_cameras: 2, management_agency_id: 1, purpose_of_camera_installation_id: 1}}
+      let(:expected) {"Validation failed: Management agency must exist, Purpose of camera installation must exist"}
+      it 'create 불가능' do
+        expect { Facility.create!(create_param) }.to raise_error(ActiveRecord::RecordInvalid, expected)
+      end
+    end
+
+    context '부모 관계인 NationalPoliceAgency를 미리 만들면' do
+      # NationalPoliceAgency
+      let(:national_police_agency_id) {NationalPoliceAgency.create!({name: 'p_agency1'}).id}
+      let(:police_station_id) {PoliceStation.create!({name: 'p_station1', national_police_agency_id:}).id}
+      let(:management_agency_id) {ManagementAgency.create!({name: "m_agency1", national_police_agency_id:, police_station_id:}).id}
+      let(:purpose_of_camera_installation_id) {PurposeOfCameraInstallation.create!({purpose: "purpose1"}).id}
+      # Facility
+      let(:create_param) {{name: 'facility1', number_of_cameras: 2, management_agency_id:, purpose_of_camera_installation_id:}}
+      let(:expected) {1}
+      it 'create 가능' do
+        Facility.create!(create_param)
+        actual = Facility.all.length
+
+        expect(actual).to equal(expected)
+      end
+    end
+  end
+end
