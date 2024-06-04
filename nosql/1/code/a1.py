2b66dc7d36f4808ae6f234dcb9f3dd11373ae26c
cheekykorkind <cheekorkind@gmail.com>
2024-06-04 09:25:20 -0400

    a2에서 필요한 함수추가, 함수 인자명 변경

--- nosql/1/code/a1.py
+++ nosql/1/code/a1.py
@@ -6,8 +6,8 @@ from dynamodb_util import *
 import pprint
 
 
-def new_user(d_util, serialized_item):
-    return d_util.put_item(serialized_item)
+def new_user(d_util, deserialized_item):
+    return d_util.put_item(deserialized_item)
 
 
 def update_user(d_util, deserialized_item):
@@ -21,6 +21,14 @@ def delete_user(d_util, deserialized_pk_sk):
 def query_user_info_by_user_id(d_util, user_id):
     q_param = {
         "KeyConditionExpression": "PK = :PK",
-        "ExpressionAttributeValues": {":PK": {"S": str(f"u#{user_id}")}},
+        "ExpressionAttributeValues": {":PK": {"S": f"u#{user_id}"}},
+    }
+    return d_util.query(q_param)
+
+
+def query_user_info_by_user_id_and_sk(d_util, user_id, sk):
+    q_param = {
+        "KeyConditionExpression": "PK = :PK AND SK = :SK",
+        "ExpressionAttributeValues": {":PK": {"S": f"u#{user_id}"}, ":SK": {"S": sk}},
     }
     return d_util.query(q_param)
