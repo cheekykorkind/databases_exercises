f57ca0179c85d584c4025e981a9e2d489b527e6a
cheekykorkind <cheekorkind@gmail.com>
2024-06-11 07:59:59 -0400

    요구사항 a6 추가

---
+++ nosql/1/code/a6.py
@@ -0,0 +1,43 @@
+from dynamodb_util import *
+from a1 import *
+import pprint
+
+
+def increase_like_count(d_util, user_id, post_id):
+    def exist_post_id(d_util, post_id):
+        return len(d_util.scan_with_sk_filter(f"p#{post_id}")) > 0
+
+    def exist_user_id(d_util, user_id):
+        return len(query_user_info_by_user_id(d_util, user_id)) > 0
+
+    if not exist_post_id(d_util, post_id) or not exist_user_id(d_util, user_id):
+        return None
+    d_util.put_item({"PK": f"p#{post_id}#likelist", "SK": f"u#{user_id}"})
+
+    from a5 import query_user_like_by_post_id
+
+    return d_util.update_item(
+        {
+            "PK": f"p#{post_id}#likecount",
+            "SK": "count",
+            "etc": len(query_user_like_by_post_id(d_util, post_id)),
+        }
+    )
+
+
+def decrease_like_count(d_util, user_id, post_id):
+    d_util.delete_item({"PK": f"p#{post_id}#likelist", "SK": f"u#{user_id}"})
+
+    from a5 import query_user_like_by_post_id
+
+    return d_util.update_item(
+        {
+            "PK": f"p#{post_id}#likecount",
+            "SK": "count",
+            "etc": len(query_user_like_by_post_id(d_util, post_id)),
+        }
+    )
+
+
+def get_like_count_by_post_id(d_util, post_id):
+    return d_util.get_item({"PK": f"p#{post_id}#likecount", "SK": "count"})
